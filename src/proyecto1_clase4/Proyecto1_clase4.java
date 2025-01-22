d23044a5a23c2285427d638c6a2c3e5f652b4847
thali <thali@Thalia_Ccoyllo>
2025-01-21 20:26:42 -0500

    Carga Inicial

--- src/proyecto1_clase4/Proyecto1_clase4.java
+++ src/proyecto1_clase4/Proyecto1_clase4.java
@@ -6,15 +6,17 @@ public class Proyecto1_clase4 {
 
     public static void main(String[] args) {
         // Declarar variables
-        int n1 ,n2 ,suma;
+        int n1 ,n2 ,n3 ,suma;
         Scanner lectura = new Scanner (System.in);
         // Entrada de Datos
         System.out.print ("Ingresar Numero 1:");
         n1=lectura.nextInt();
         System.out.print ("Ingresar Numero 2:");
         n2=lectura.nextInt();
+        System.out.print ("Ingresar Numero 3:");
+        n3=lectura.nextInt();
         // proceso de datos
-        suma = n1+n2;
+        suma = n1+n2+n3;
         //Salida de datos
         System.out.println("La Suma de los datos es:" +suma);
         
